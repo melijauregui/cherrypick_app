8948f47c95a8c4a6e9bf192ba770a6648e7a46ec
Sofia-gb <sofiagomezb@yahoo.com.ar>
2025-04-28 17:06:43 -0300

    [WIP] balance types of jeans: wip: palazzo con rotura y mom

--- database/count.py
+++ database/count.py
@@ -4,13 +4,13 @@ from collections import defaultdict
 # Definí las palabras clave que vas a buscar
 cortes = {
     # "liso",
-    # "jean wide leg": [0, 0],
-    # "jean skinny": [0, 0],
-    # "jean recto": [0, 0],
+    "jean wide leg": [0, 0],
+    "jean skinny": [0, 0],
+    "jean recto": [0, 0],
     "jean palazzo": [0, 0],
     "jean cargo": [0, 0],
-    "jean mom ": [0, 0],
-    # "jean acampanado": [0, 0],
+    "jean mom": [0, 0],
+    "jean acampanado": [0, 0],
 }
 
 df = pd.read_csv('datasets/con-sin-roturas-v3.csv')
@@ -26,4 +26,6 @@ for desc in df['description']:
 
 for corte, _ in cortes.items():
     con_rotura, sin_rotura = cortes[corte]
+    if con_rotura == 25 and sin_rotura == 25:
+        continue
     print(f"{corte}: {con_rotura} con rotura, {sin_rotura} sin rotura")
--- database/datasets/convertir_csv.py
+++ database/datasets/convertir_csv.py
@@ -13,17 +13,26 @@ def limpiar_descripcion(texto):
 
     texto = re.sub(r'\bsin rotura\b', '', texto, flags=re.IGNORECASE)
 
-    texto = re.sub(r'recto', 'RECTO', texto, flags=re.IGNORECASE)
-    texto = re.sub(r'wide leg', 'WIDE LEG', texto, flags=re.IGNORECASE)
-    texto = re.sub(r'skinny', 'SKINNY', texto, flags=re.IGNORECASE)
-    texto = re.sub(r'palazzo', 'PALAZZO', texto, flags=re.IGNORECASE)
-    texto = re.sub(r'cargo', 'CARGO', texto, flags=re.IGNORECASE)
-    texto = re.sub(r'acampanado', 'ACAMPANADO', texto, flags=re.IGNORECASE)
-    texto = re.sub(r'roturas', 'ROTURAS', texto, flags=re.IGNORECASE)
-    texto = re.sub(r'rotura', 'ROTURA', texto)
+    texto = re.sub(r'recto', 'RECTO', texto)
+    texto = re.sub(r'wide leg', 'WIDE LEG', texto)
+    texto = re.sub(r'skinny', 'SKINNY', texto)
+    texto = re.sub(r'palazzo', 'PALAZZO', texto)
+    texto = re.sub(r'cargo', 'CARGO', texto)
+    texto = re.sub(r'acampanado', 'ACAMPANADO', texto)
+    texto = re.sub(r'flared o CAMPANA', 'ACAMPANADO',
+                   texto, flags=re.IGNORECASE)
+    texto = re.sub(r'flatered o CAMPANA', 'ACAMPANADO',
+                   texto, flags=re.IGNORECASE)
+    texto = re.sub(r'flared', 'ACAMPANADO', texto)
 
-    texto = re.sub(r'flatered', 'FLATERED', texto, flags=re.IGNORECASE)
-    texto = re.sub(r'campana', 'CAMPANA', texto, flags=re.IGNORECASE)
+    # texto = re.sub(r'FLATERED o CAMPANA', 'ACAMPANADO', textoflags=re.IGNORECASE)
+    texto = re.sub(r' mom ', ' MOM ', texto)
+    texto = re.sub(r'roturas', 'ROTURAS', texto)
+    texto = re.sub(r'rotura', 'ROTURA', texto)
+    texto = re.sub(r'jean  ', 'jean ', texto)
+
+    # texto = re.sub(r'flatered', 'FLATERED', texto, flags=re.IGNORECASE)
+    # texto = re.sub(r'campana', 'CAMPANA', texto, flags=re.IGNORECASE)
     # texto = re.sub(r'\s+', ' ', texto).strip()
 
     return texto
@@ -36,14 +45,8 @@ with open(input_file, 'r', newline='', encoding='utf-8') as infile, \
     writer = csv.writer(outfile)
 
     for row in reader:
-        if len(row) != 2:
-            continue
         url, descripcion = row
         descripcion_limpia = limpiar_descripcion(descripcion)
         writer.writerow([url, descripcion_limpia])
-
-    df = pd.read_csv(output_file, names=["image", "description"])
-    print(df['description'].str.contains('rotura', case=False).value_counts())
-
 
 print(f"Archivo limpio guardado en {output_file}")
