aea1038f718b25e88d368bf028551098e1a399e3
Sofia-gb <sofiagomezb@yahoo.com.ar>
2025-08-18 11:48:48 -0300

    [FIX] android

--- app.config.js
+++ app.config.js
@@ -4,6 +4,9 @@ export default {
         slug: "cherrypick",
         scheme: "cherrypick",
         version: "1.0.0",
+        android: {
+            package: "com.cherrypickapp.cherrypick",
+        },
         extra: {
             LOCAL_IP: process.env.LOCAL_IP || "localhost",
             BETTER_AUTH_URL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
